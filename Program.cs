d21288c715f8e805f01df50065119bccb5da8e53
Levente Major <leventemajor36@gmail.com>
2023-11-10 10:44:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FeleviEredmenyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new List<Tanulo>();
+            using StreamReader sr = new StreamReader(path: @"..\..\..\src\data.txt", Encoding.UTF8);
+            List<string> definiciok = new List<string>();
+
+            string sep = "\t";
+            var nevek = sr.ReadLine().Split(sep.ToCharArray());
+            foreach (var item in nevek)
+            {
+                definiciok.Add(item);
+            }
+
+            while (!sr.EndOfStream)
+            {
+                _ = sr.ReadLine();
+                tanulok.Add(new Tanulo(sr.ReadLine()));
+            };
+
+            foreach (var item in definiciok)
+            {
+                Console.Write($"{item},");
+            }
+            foreach (var item in tanulok)
+            {
+                Console.WriteLine(item);
+            }
+            //1. feladat
+            //Írjunk függvényt, ami kiszámítja a jegyek átlagát tanulónként.Írjunk függvényt, ami kiszámítja az osztályátlagot, illetve a tantárgyankénti átlagot is.
+            //2. feladat: Programozás gyakorlatból megbukottak adatainak kiiratása.
+            //3. feladat: Írjunk függvényt, amivel keressük meg az első olyan embert, akinek hármasa van angol nyelvből, majd írjuk ki az adatait.
+            //4. feladat: Kérjük be a felhasználótól, melyik tanuló legjobb jegyét szeretné megtudni.
+            //Írjuk ki az adott tanuló legjobb eredményét függvénnyel. Szorgalmi: Kezeljük a lehetséges felhasználói hibát is.
+            //5. feladat: Egy új fájlba írjuk ki a fenti tanuló nevét és oktatási azonosítóját.
+        }
+    }
+}
---
+++ Tanulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeleviEredmenyek
+{
+    class Tanulo
+    {
+        //Tanuló neve Oktatási azonosító  Hálózatok I.Hálózatok I.gyakorlat Programozás Programozás gyakorlat Angol nyelv Magyar nyelv és irodalom  Matematika Történelem
+        public string Nev { get; set; }
+        public double Azonosito { get; set; }
+        public List<int> Ertekelesek { get; set; }
+
+        public Tanulo(string line)
+        {
+            string sep = "\t";
+            var pieces = line.Split(sep.ToCharArray());
+            this.Nev = pieces[0];
+            this.Azonosito = double.Parse(pieces[1]);
+            for (int i = 2; i < pieces.Length ; i++)
+            {
+                this.Ertekelesek.Add(int.Parse(pieces[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Nev}, {this.Azonosito}, {this.Ertekelesek}";
+        }
+    }
+}
