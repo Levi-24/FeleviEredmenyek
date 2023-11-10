06c500a0230e133fe9b3024fb822fb80b1dd97c4
Levente Major <leventemajor36@gmail.com>
2023-11-10 11:04:43 +0100

    1 f

--- Program.cs
+++ Program.cs
@@ -27,21 +27,36 @@ namespace FeleviEredmenyek
                 tanulok.Add(new Tanulo(sr.ReadLine()));
             };
 
-            foreach (var item in definiciok)
-            {
-                Console.Write($"{item},");
-            }
-            foreach (var item in tanulok)
-            {
-                Console.WriteLine(item);
-            }
             //1. feladat
             //Írjunk függvényt, ami kiszámítja a jegyek átlagát tanulónként.Írjunk függvényt, ami kiszámítja az osztályátlagot, illetve a tantárgyankénti átlagot is.
+            Console.WriteLine("1.Feladat: Tanulók átlagjai:");
+            foreach (var tanulo in tanulok)
+            {
+                Console.WriteLine(TanuloAtlag(tanulo));
+            }
+            Console.WriteLine("Osztály átlagok:");
+            Console.WriteLine(OsztalyAtlag(tanulok));
             //2. feladat: Programozás gyakorlatból megbukottak adatainak kiiratása.
             //3. feladat: Írjunk függvényt, amivel keressük meg az első olyan embert, akinek hármasa van angol nyelvből, majd írjuk ki az adatait.
             //4. feladat: Kérjük be a felhasználótól, melyik tanuló legjobb jegyét szeretné megtudni.
             //Írjuk ki az adott tanuló legjobb eredményét függvénnyel. Szorgalmi: Kezeljük a lehetséges felhasználói hibát is.
             //5. feladat: Egy új fájlba írjuk ki a fenti tanuló nevét és oktatási azonosítóját.
+        }
+        static double TanuloAtlag(Tanulo tanulo)
+        {
+            return tanulo.Ertekelesek.Average(v => v);
+        }
+        static double OsztalyAtlag(List<Tanulo> tanulok)
+        {
+            int tanuloCount = tanulok.Count();
+            double[] tanuloAtlagok= new double[tanuloCount];
+
+            for (int i = 0; i < tanuloCount; i++)
+            {
+                tanuloAtlagok[i] = tanulok[i].Ertekelesek.Average(v => v);
+            }
+
+            return tanuloAtlagok.Average();
         }
     }
 }
